8eaeb5f541bad6353f49e617ded60d6c55a801cf
Dovi Klausner <dovi.klausner@autodesk.com>
2021-11-06 22:01:11 +0200

    reformatted the file

--- frontend/app_data.py
+++ frontend/app_data.py
@@ -1,21 +1,21 @@
 products = [
-        {'product': 'car', 'price': '50000'},
-        {'product': 'shirt', 'price': '100'},
-        {'product': 'hat', 'price': '20'},
-        {'product': 'apple', 'price': '5'},
-        {'product': 'cookie', 'price': '5'},
-        {'product': 'football', 'price': '35'},
-        {'product': 'watch', 'price': '200'},
-        {'product': 'computer', 'price': '2000'}
-    ]
+    {'product': 'car', 'price': '50000'},
+    {'product': 'shirt', 'price': '100'},
+    {'product': 'hat', 'price': '20'},
+    {'product': 'apple', 'price': '5'},
+    {'product': 'cookie', 'price': '5'},
+    {'product': 'football', 'price': '35'},
+    {'product': 'watch', 'price': '200'},
+    {'product': 'computer', 'price': '2000'}
+]
 
 users = [
-        {'name': 'Leonard', 'id': 543253},
-        {'name': 'Sheldon', 'id': 32132},
-        {'name': 'Howard', 'id': 908797},
-        {'name': 'Raj', 'id': 87654},
-        {'name': 'Penny', 'id': 20496},
-        {'name': 'Amy', 'id': 8764},
-        {'name': 'Berny', 'id': 54367},
-        {'name': 'Anu', 'id': 32543}
-    ]
+    {'name': 'Leonard', 'id': 543253},
+    {'name': 'Sheldon', 'id': 32132},
+    {'name': 'Howard', 'id': 908797},
+    {'name': 'Raj', 'id': 87654},
+    {'name': 'Penny', 'id': 20496},
+    {'name': 'Amy', 'id': 8764},
+    {'name': 'Berny', 'id': 54367},
+    {'name': 'Anu', 'id': 32543}
+]
